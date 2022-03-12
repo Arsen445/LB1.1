679e7289fdbc557cda6d9c34b36edeb8304287d7
Arsen445 <arsen454545454@gmail.com>
2022-03-12 23:48:47 +0300

    Добавлен комит 7

--- asd.py
+++ asd.py
@@ -4,3 +4,5 @@ for y in 0, 1:
         for z in 0, 1:
             for w in 0, 1:
                 F = (not(x) or y or z) and (x or not(z) or not(w))
+                        if not(F):
+                        print(x, y, z, w)
